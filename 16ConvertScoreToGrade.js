2cebbd4928ea250313837b2269d7ef630b1d7372
camila <camiramirezpp@gmail.com>
2018-06-11 11:13:45 -0400

    arreglo al ejercicio 16

--- 16ConvertScoreToGrade.js
+++ 16ConvertScoreToGrade.js
@@ -5,16 +5,16 @@ function ConvertScoreToGrade(num) {
   if(num < 100 && num > 90){
     return "a";
   }
-  if(num < 89 && num > 80){
+  if(num <= 89 && num > 80){
     return "b";
   }
-  if(num < 79 && num > 70){
+  if(num <= 79 && num > 70){
     return "c";
   }
-  if(num < 69 && num > 60){
+  if(num <= 69 && num > 60){
     return "d";
   }
-  if(num < 59 && num > 0){
+  if(num <= 59 && num > 0){
     return "f";
   }
   else if ( num > 100 || num < 0) {
